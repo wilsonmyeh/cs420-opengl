c871a130c20362769388df5c4f357ff658528404
Wilson Yeh <wilsonye@usc.edu>
2017-02-24 23:07:34 -0800

    hw2 milestone (draw spline) & texture mapping

--- hw2/openGLHelper-starterCode/basic.fragmentShader.glsl
+++ hw2/openGLHelper-starterCode/basic.fragmentShader.glsl
@@ -1,11 +1,14 @@
 #version 150
 
-in vec4 col;
-out vec4 c;
+in vec2 tc; // input tex coordinates (computed by the interpolator)
+
+out vec4 c; // output color (the final fragment color)
+
+uniform sampler2D textureImage; // the texture image
 
 void main()
 {
-  // compute the final pixel color
-  c = col;
-}
-
+ // compute the final fragment color,
+ // by looking up into the texture map
+ c = texture(textureImage, tc);
+} 
